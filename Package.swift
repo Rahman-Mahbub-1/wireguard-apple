d1acc9cebecc459fb0343c4d635d355605dce8cd
Rahman-Mahbub-1 <mahbubur.asia.bangladesh@avianbpo.com>
2025-11-05 15:19:19 +0600

    Make WireGuardKit SPM compatible

---
+++ Package.swift
@@ -0,0 +1,54 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WireGuardKit",
+    platforms: [
+        .macOS(.v11),
+        .iOS(.v14)
+    ],
+    products: [
+        .library(name: "WireGuardKit", targets: ["WireGuardKit"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "WireGuardKit",
+            dependencies: ["WireGuardKitGo", "WireGuardKitC"],
+            path: "Sources/WireGuardKit"
+        ),
+        .target(
+            name: "WireGuardKitC",
+            dependencies: [],
+            path: "Sources/WireGuardKitC",
+            publicHeadersPath: "."
+        ),
+        .target(
+            name: "WireGuardKitGo",
+            dependencies: [],
+            path: "Sources/WireGuardKitGo",
+            exclude: [
+                "goruntime-boottime-over-monotonic.diff",
+                "go.mod",
+                "go.sum", 
+                "api-apple.go",
+                "Makefile",
+                ".tmp",
+                ".gitignore",
+                "out"
+            ],
+            sources: ["dummy.c"],
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("."),
+                .define("SWIFT_PACKAGE")
+            ],
+            linkerSettings: [
+                .linkedLibrary("wg-go"),
+                .unsafeFlags(["-L."], .when(platforms: [.iOS, .macOS]))
+            ]
+        )
+    ]
+)
